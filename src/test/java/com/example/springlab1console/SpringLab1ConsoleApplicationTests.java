1e1f6c438947dce0385ff5719a4907c161ebb795
Pashhaa <71789749+Pashhaa@users.noreply.github.com>
2022-09-20 01:10:27 +0300

    Initial commit

---
+++ src/test/java/com/example/springlab1console/SpringLab1ConsoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springlab1console;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLab1ConsoleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
